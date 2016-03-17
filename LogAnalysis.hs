cd8be3c726249e7c6bd8497d16a7d49faa95e2a8
dinnu93 <dinesh.padisetti@gmail.com>
2016-03-18 02:17:55 +0530

    Completed chapter-2 exercise

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -25,3 +25,33 @@ parse :: String -> [LogMessage]
 parse s = map parseMessage $ lines s
 
 -- Exercise-2
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) msgTree = msgTree
+insert logMsg Leaf = Node Leaf logMsg Leaf
+insert logMsg@(LogMessage _ t _) msgTree@(Node lMsgTree nLogMessage@(LogMessage _ nT _) rMsgTree)
+  | t < nT = Node (insert logMsg lMsgTree) nLogMessage rMsgTree
+  | otherwise = Node lMsgTree nLogMessage (insert logMsg rMsgTree)
+
+-- Exercise-3
+
+build :: [LogMessage] -> MessageTree
+build logMsgList = foldr insert Leaf logMsgList
+
+-- Exercise-4
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node lMsgTree logMsg rMsgTree) = inOrder lMsgTree ++ [logMsg] ++ inOrder rMsgTree
+
+-- Exercise-5
+
+allowedLogMessage :: LogMessage -> Bool
+allowedLogMessage (Unknown _) = False
+allowedLogMessage (LogMessage (Error s) _ _)
+  | s>50 = True
+  | otherwise = False
+allowedLogMessage (LogMessage _ _ _) = False
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong logMsgList = map (\logMsg@(LogMessage _ _ msg) -> msg)  $ filter allowedLogMessage logMsgList
