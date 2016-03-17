83bd53145c0cbec867de567070661ef646ea98a5
dinnu93 <dinesh.padisetti@gmail.com>
2016-03-18 00:28:33 +0530

    JUst refactored a little

--- LogAnalysis.hs
+++ LogAnalysis.hs
@@ -3,17 +3,25 @@ import Log
 
 -- CIS 194 Chapter-2 Homework
 
+-- Exercise-1
+
 parseMessage :: String -> LogMessage
 parseMessage s
   | null wordList = Unknown s
   | length firstWord /= 1 = Unknown s
-  | firstWord == "E" = LogMessage (Error (read (wordList !! 1) :: Int)) (read (wordList !! 2) :: Int) (unwords (drop 3 wordList)) 
-  | firstWord == "I" = LogMessage Info (read (wordList !! 1) :: Int) (unwords (drop 2 wordList))
-  | firstWord == "W" = LogMessage Warning (read (wordList !! 1) :: Int) (unwords (drop 2 wordList))
+  | firstWord == "E" = LogMessage (Error firstNumber) secondNumber errorMsg
+  | firstWord == "I" = LogMessage Info firstNumber otherMsg
+  | firstWord == "W" = LogMessage Warning firstNumber otherMsg
   | otherwise = Unknown s
   where wordList = words s
         firstWord = head wordList
+        firstNumber =  read (wordList !! 1) :: Int
+        secondNumber = read (wordList !! 2) :: Int
+        errorMsg = unwords $ drop 3 wordList
+        otherMsg = unwords $ drop 2 wordList
 
-
+        
 parse :: String -> [LogMessage]
 parse s = map parseMessage $ lines s
+
+-- Exercise-2
