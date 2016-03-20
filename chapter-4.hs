e57632dbf095df273fe7e3a2f5a147da3d1f8ff6
dinnu93 <dinesh.padisetti@gmail.com>
2016-03-20 22:28:44 +0530

    Completed exercise 1

---
+++ chapter-4.hs
@@ -0,0 +1,27 @@
+-- Chapter-4 Exercises
+
+-- Exercise-1
+
+fun1 :: [Integer] -> Integer
+fun1 []     = 1
+fun1 (x:xs)
+  | even x    = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (flip (-) 2) . filter even
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n    = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (/=1) . iterate collatz
+  where collatz n
+          | even n = div n 2
+          | otherwise = 3 * n + 1
+            
+-- Exercise-2
+
