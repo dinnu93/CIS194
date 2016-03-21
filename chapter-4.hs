358de2138683513a36d16f77399bc2864b2ed98a
dinnu93 <dinesh.padisetti@gmail.com>
2016-03-21 23:12:08 +0530

    Trying to solve exercise 2

--- chapter-4.hs
+++ chapter-4.hs
@@ -25,3 +25,26 @@ fun2' = sum . filter even . takeWhile (/=1) . iterate collatz
             
 -- Exercise-2
 
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+            deriving (Show, Eq)
+
+treeHeight :: Tree a -> Integer
+treeHeight Leaf = -1
+treeHeight (Node h _ _ _) = h
+
+balancedTree :: Tree a -> Bool
+balancedTree Leaf = True
+balancedTree (Node h lTree node rTree) = (balancedTree lTree) && (balancedTree rTree) && balHeight
+  where balHeight = abs ((treeHeight lTree) - (treeHeight rTree)) <= 1
+
+
+insert :: a -> Tree a -> Tree a
+insert x Leaf = Node 0 Leaf x Leaf
+insert x tree@(Node h lTree node rTree)
+  | min (treeHeight lTree) (treeHeight rTree) == (treeHeight lTree) =  Node (h+1) (insert x lTree) node rTree
+  | otherwise = Node (h+1) lTree node (insert x rTree)
+
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf  
+
