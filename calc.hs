e8cc54e0d1c163892780b92e662f15983b808b43
dinnu93 <dinesh.padisetti@gmail.com>
2016-04-06 18:59:10 +0530

    Completed exercise-4

--- calc.hs
+++ calc.hs
@@ -29,5 +29,38 @@ instance Expr ExprT where
   add x y = Add x y
   mul x y = Mul x y
 
+-- Exercise-4
 
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+  
  
+instance Expr Bool where
+  lit x = if x < 0 then False else True
+  add = (||)
+  mul = (&&)
+
+newtype MinMax  = MinMax Integer deriving (Eq, Show)
+newtype Mod7    = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+instance Expr Mod7 where
+  lit x = Mod7 (mod x 7)
+  add (Mod7 x) (Mod7 y) = Mod7 (mod (x + y) 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 (mod (x * y) 7)
+
+
+-- Testing
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
+testInteger  = testExp :: Maybe Integer
+testBool     = testExp :: Maybe Bool
+testMM       = testExp :: Maybe MinMax
+testSat      = testExp :: Maybe Mod7
